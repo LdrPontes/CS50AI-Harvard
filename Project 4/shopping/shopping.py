bf500fff2a93ab9330b6eabd1b0f9faabba67a20
Leandro Pontes Berleze <leandropberleze@gmail.com>
2021-01-18 18:10:13 -0300

    Finished Project 4A

--- Project 4/shopping/shopping.py
+++ Project 4/shopping/shopping.py
@@ -59,7 +59,68 @@ def load_data(filename):
     labels should be the corresponding list of labels, where each label
     is 1 if Revenue is true, and 0 otherwise.
     """
-    raise NotImplementedError
+    raw_data = list()
+
+    labels = list()
+    evidences = list()
+
+    with open(filename, newline='') as csvfile:
+        reader = csv.reader(csvfile,delimiter=",", )
+        for i in reader:
+            raw_data.append(i)
+        raw_data.pop(0)
+
+    for row in raw_data:
+        labels.append(int(row[len(row) - 1] == "TRUE"))
+        evidence = list()
+        for i in range(17):
+            evidence.append(convert_evidences(row[i], i))
+        evidences.append(evidence)
+
+    return (evidences, labels)
+
+
+def convert_evidences(value, position):
+    int_positions = [0, 2, 4, 10, 11, 12, 13, 14, 15, 16]
+    float_positions = [1, 3, 5, 6, 7, 8, 9]
+
+    if(position in int_positions):
+        if(position == 10):
+            return int(month_to_int(value))
+        if(position == 15):
+            return int(value == "Returning_Visitor")
+        if(position == 16):
+            return int(value == "TRUE")
+        return int(value)
+    else:
+        return float(value)
+
+
+def month_to_int(month):
+    if(month == "Jan"):
+        return 0
+    if(month == "Feb"):
+        return 1
+    elif(month == "Mar"):
+        return 2
+    elif(month == "Apr"):
+        return 3
+    elif(month == "May"):
+        return 4
+    elif(month == "June"):
+        return 5
+    elif(month == "Jul"):
+        return 6
+    elif(month == "Aug"):
+        return 7
+    elif(month == "Sep"):
+        return 8
+    elif(month == "Oct"):
+        return 9
+    elif(month == "Nov"):
+        return 10
+    else:
+        return 11
 
 
 def train_model(evidence, labels):
@@ -67,7 +128,10 @@ def train_model(evidence, labels):
     Given a list of evidence lists and a list of labels, return a
     fitted k-nearest neighbor model (k=1) trained on the data.
     """
-    raise NotImplementedError
+    model = KNeighborsClassifier(n_neighbors=1)
+    x_training = [item for item in evidence]
+    y_training = [item for item in labels]
+    return model.fit(x_training, y_training)
 
 
 def evaluate(labels, predictions):
@@ -85,7 +149,26 @@ def evaluate(labels, predictions):
     representing the "true negative rate": the proportion of
     actual negative labels that were accurately identified.
     """
-    raise NotImplementedError
+    count_positive = 0
+    count_negative = 0
+
+    count_sensitivity = 0
+    count_specificity = 0
+
+    for i in range(len(labels)):
+        if(labels[i] == 1):
+            count_positive += 1
+            if(labels[i] == predictions[i]):
+                count_sensitivity += 1
+        else:
+            count_negative += 1
+            if(labels[i] == predictions[i]):
+                count_specificity += 1
+
+    sensitivity = count_sensitivity / count_positive
+    specificity = count_specificity / count_negative
+
+    return (sensitivity, specificity)
 
 
 if __name__ == "__main__":
